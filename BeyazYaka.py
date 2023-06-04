27c9c6e687716f23928f576af8db4bf0575ed33f
ahsengokalp <128702801+ahsengokalp@users.noreply.github.com>
2023-06-04 13:12:15 +0300

    BeyazYaka.py Güncellendi
    zam_hakkı fonksiyonu güncellendi. get_yeni_maas fonksiyonu eklendi

--- BeyazYaka.py
+++ BeyazYaka.py
@@ -12,17 +12,23 @@ class BeyazYaka(Calisan):
         self.__tesvik_primi = tesvik_primi
 
     def zam_hakki(self):
-        if self.get_tecrube() < 2:
-            return self.__tesvik_primi
-        elif 2 <= self.get_tecrube() <= 4 and self.get_maas() < 15000:
-            return (self.get_maas() * self.get_tecrube()) * 5 + self.__tesvik_primi
-        elif self.get_tecrube() > 4 and self.get_maas() < 25000:
-            return (self.get_maas() * self.get_tecrube()) * 4 + self.__tesvik_primi
-        else:
-            return self.get_maas()
+        try:
+            if self.get_tecrube() < 2:
+                return self.__tesvik_primi
+            elif 2 <= self.get_tecrube() <= 4 and self.get_maas() < 15000:
+                return (self.get_maas() * self.get_tecrube() / 100) * 5 + self.__tesvik_primi
+            elif self.get_tecrube() > 4 and self.get_maas() < 25000:
+                return (self.get_maas() * self.get_tecrube() / 100) * 4 + self.__tesvik_primi
+            else:
+                return 0
+
+        except Exception as e:#hata bulunması durumu
+            print("hata-", str(e))
+    def get_yeni_maas(self):
+        return self.get_maas() + self.zam_hakki()
 
     def __str__(self):
-        return super().__str__() + f"Teşvik Prim: {self.__tesvik_primi}\n"
+        return f"Ad:{self.get_ad()}\nSoyad:{self.get_soyad()}\nTecrübe:{self.get_tecrube()}ay\nYeni Maaş:{yeni_maas}TL"
 
 
 

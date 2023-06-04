c02b2558e65e2986e7330c0acf84629873e5f8cd
ahsengokalp <128702801+ahsengokalp@users.noreply.github.com>
2023-06-04 12:44:53 +0300

    Calisan ve Issiz class ve nesneleri oluşturulacak
    Insan classından Klıtım yoluyla oluşturulacak

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from Insan import Insan
+
+insan1 = Insan("12345678910", "Ahmet", "Yılmaz", 30, "Erkek", "Türk")
+insan2 = Insan("98765432100", "Aliye", "Ata", 22, "Kadın", "Türk")
+print(insan1)
+print(insan2)
